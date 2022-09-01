b02cac544f3989bb7e70e2b25d286366b1544a4b
Ayush Rudani <78534043+R-Ayush777@users.noreply.github.com>
2022-09-01 09:36:26 +0000

    Navbar Done

---
+++ client/devcube/src/components/navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+
+function Navbar() {
+  return (
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">DebCube</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse d-flex" id="navbarSupportedContent">
+          <div class="">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+            </ul>
+          </div>
+          <div class="">
+            <div class="input-group ms-3">
+              <input type="text" class="form-control" placeholder="Search" />
+              <span class="input-group-text">icon</span>
+            </div>
+          </div>
+          <div class="ms-auto">
+            <button type="button" class="btn btn-light">Log in</button>
+            <button type="button" class="ms-3 btn btn-outline-primary">Create account</button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+export default Navbar;
--- client/devcube/src/index.js
+++ client/devcube/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +16,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
