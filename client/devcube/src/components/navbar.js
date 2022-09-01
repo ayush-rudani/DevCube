2c47d6ba7102a05e5d10d55d730ec9c627e79eab
Ayush Rudani <78534043+R-Ayush777@users.noreply.github.com>
2022-09-01 10:02:00 +0000

    Navbar-1

--- client/devcube/src/components/navbar.js
+++ client/devcube/src/components/navbar.js
@@ -5,19 +5,21 @@ function Navbar() {
   return (
     <nav class="navbar navbar-expand-lg bg-light">
       <div class="container-fluid">
-
         <a class="navbar-brand" href="#">DebCube</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse d-flex" id="navbarSupportedContent">
-          <div class="">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
+
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <div class="">
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-            </ul>
-          </div>
+              </div>
+            </li>
+          </ul>
+
+
           <div class="">
             <div class="input-group ms-3">
               <input type="text" class="form-control" placeholder="Search" />
