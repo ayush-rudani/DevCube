f98ce3808ca1e8f2f06c8bc217a7723b4898d12e
Ayush Rudani <78534043+R-Ayush777@users.noreply.github.com>
2022-09-01 09:55:13 +0000

    N2

---
+++ client/devcube/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar.js';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Navbar />
+
+    </div >
+  );
+}
+
+export default App;
