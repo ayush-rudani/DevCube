57eba7de92014c0eb2269cbb31e874e149001cfe
Ayush Rudani <78534043+R-Ayush777@users.noreply.github.com>
2022-09-10 08:39:24 +0000

    M

--- client/devcube/src/components/navbar.js
+++ client/devcube/src/components/navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <nav class="navbar navbar-expand-lg bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">DebCube</a>
+        <a class="navbar-brand" href="#">DevCube</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
