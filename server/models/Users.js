d134ea4989c0626f62d555d8a9403852313ad947
Ayush Rudani <ayushcrudani@gmail.com>
2022-09-11 14:41:42 +0530

    M user models

--- server/models/Users.js
+++ server/models/Users.js
@@ -6,9 +6,29 @@ const Schema = new mongoose.Schema();
 // Model is a class with which we construct documents. In this case, each document will be a user with properties and behaviors as declared in our schema.
 
 const userSchema = new Schema({
-    name: { String, required: true },
-    email: { String, required: true },
-    password: { String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minlength: 6 },
+
+    // Basic Information about user     -----------------
+    Bio: { type: String, required: false },
+    website: { type: String, required: false },
+    // location: { type: String, required: false },
+
+    // Social Media Links   ----------
+    // twitter: { type: String, required: false },
+    // linkedin: { type: String, required: false },
+    // github: { type: String, required: false },
+
+    // Coidng   ---------------
+    // skills: { type: [String], required: false },
+    // status: { type: String, required: false },
+
+    // Work     ------------------
+    // work: { type: String, required: false },
+    // education: { type: String, required: false },
+
 });
 
 module.exports = mongoose.model('User', userSchema);
