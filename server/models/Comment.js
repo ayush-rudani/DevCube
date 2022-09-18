1e2913ec120de6142e64e7cecaa6c9954ccabfe2
Ayush Rudani <ayushcrudani@gmail.com>
2022-09-18 14:09:36 +0530

    Comment model added

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -2,14 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-  body : {type: String, required: true},
-  user : {type : Schema.Types.ObjectId, ref: 'User', required: true},
-  post : [ type : Schema.Types.ObjectId, ref: 'Post', required: true },
-            
-  
-  
-  
-    
+  body: { type: String, required: true },
+  date: { type: Date, default: Date.now },
+  post: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
+  parentC: { type: Schema.Types.ObjectId, ref: 'Comment', default: null },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 });
 
 
