0941ca5a1e9afa05b0a9378dd5d007cac11a3653
Ayush Rudani <ayushcrudani@gmail.com>
2022-10-31 21:03:56 +0530

    Navbar scss done

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,10 +2,13 @@ function Register() {
     return (
         <>
             <div className="row mt-80">
-                <div className="col-8"> </div>
-            </div>
-        </>
-    );
+                <div className="col-8">
+                    <div className="bgImage"></div>
+                    <div className="col-4">
+                    </div>
+                </div>
+            </>
+            );
 }
 
-export default Register;
+            export default Register;
