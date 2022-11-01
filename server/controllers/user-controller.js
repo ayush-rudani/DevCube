84be1d782d4edd65b61523bf67cd541b58427d68
Ayush Rudani <ayushcrudani@gmail.com>
2022-11-01 22:54:00 +0530

    Register & Login Final DONE

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,6 +1,7 @@
 const User = require('../models/Users.js');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { body, validationResult, check } = require('express-validator');
 
 
 // module.exports.registerValiations = [
@@ -25,10 +26,24 @@ const getAllUser = async (req, res, nxt) => {
   return res.status(200).json({ users });
 }
 
+const registerValiations = [
+  body('name').not().isEmpty().trim().withMessage('Name is required'),
+  body('email').not().isEmpty().trim().withMessage('Email is required'),
+  body('password')
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters long'),
+];
 
 // router.post("/signup", userC.signup);
 const signup = async (req, res, nxt) => {
   const { name, email, username, password } = req.body;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   let existingUser;
   try {
     existingUser = await User.findOne({ email: email });
@@ -36,7 +51,7 @@ const signup = async (req, res, nxt) => {
     console.log(err);
   }
   if (existingUser) {
-    return res.status(422).json({ message: 'User already exists' });
+    return res.status(422).json({ errors: [{ msg: 'Email is already taken' }] });
   }
   // const hashedPassword = bcrypt.hashSync(password);
   let hashedPassword
@@ -61,32 +76,63 @@ const signup = async (req, res, nxt) => {
   return res.status(201).json({ _id: newUser.id, user: newUser, token: generateToken(newUser), message: 'User created successfully' });
 }
 
+const loginValiations = [
+  // check('email').not().isEmpty().trim().withMessage('Email is required'),
+  // check('password').not().isEmpty().trim().withMessage('Password is required'),
+  body('email').not().isEmpty().trim().withMessage('Email is required'),
+  body('password').not().isEmpty().withMessage('Password is required'),
+];
 
-// router.post("/login", userC.login);
 const login = async (req, res, nxt) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   const { email, password } = req.body;
-  let existingUser;
+
   try {
-    existingUser = await User.findOne({ email: email });
-    console.log(existingUser);
-  } catch (err) {
-    return console.log(err);
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+      const matched = await bcrypt.compare(password, existingUser.password);
+      if (matched) {
+        return res.status(200).json({ _id: existingUser.id, user: existingUser, token: generateToken(existingUser), message: 'User logged in successfully' });
+      }
+      else {
+        return res.status(401).json({ errors: [{ msg: 'Password is not correct' }] });
+      }
+    }
+    else {
+      return res.status(404).json({ errors: [{ msg: 'User not found' }] });
+    }
   }
-  if (!existingUser) {
-    return res.status(404).json({ message: 'User not found' });
+  catch (err) {
+    console.log(err);
+    return res.status(500).json({ errors: err });
   }
 
+  //////////////// LAST  /////////////////////////////
+  // let existingUser;
+  // try {
+  //   existingUser = await User.findOne({ email: email });
+  //   console.log(existingUser);
+  // } catch (err) {
+  //   return console.log(err);
+  // }
+  // if (!existingUser) {
+  //   return res.status(404).json({ message: 'User not found' });
+  // }
+  // //validate password
+  // let isValidPassword = false;
+  // try {
+  //   isValidPassword = await bcrypt.compare(password, existingUser.password);
+  // } catch (err) {
+  //   return console.log(err);
+  // }
+  // res.status(200).json({ message: 'Logged in', _id: existingUser.id, token: generateToken(existingUser._id) });
 
-
-  //validate password
-  let isValidPassword = false;
-  try {
-    isValidPassword = await bcrypt.compare(password, existingUser.password);
-  } catch (err) {
-    return console.log(err);
-  }
-  res.status(200).json({ message: 'Logged in', _id: existingUser.id, token: generateToken(existingUser._id) });
-
+  //////////////////////////////////////////////////////////////////
 
   // const isValidPassword = await bcrypt.compare(password, existingUser.password);
   // if (!isValidPassword) {
@@ -138,4 +184,4 @@ const generateToken = (user) => {
   return jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: '30d' });
 };
 
-module.exports = { getAllUser, signup, login, getMe };
+module.exports = { getAllUser, signup, login, getMe, registerValiations, loginValiations };
--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const userC = require("../controllers/user-controller.js");
 const { protect } = require("../middleware/auth.js");
+const { loginValiations, registerValiations } = require("../controllers/user-controller.js");
 
 router.get("/", userC.getAllUser);
-router.post("/signup", userC.signup);
-router.post("/login", userC.login);
+router.post("/signup", registerValiations, userC.signup);
+router.post("/login", loginValiations, userC.login);
 // router.put("/user:id", userC.updateUser);
 router.get("/me", protect, userC.getMe);
 
